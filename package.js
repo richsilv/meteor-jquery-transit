2a227877d482e54eeccb8d5e9b5c778a1f8e4f5d
Richard Silverton <richsilv@yahoo.co.uk>
2014-07-29 18:18:44 +0100

    Update package.js

--- package.js
+++ package.js
@@ -3,8 +3,6 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.add_files([
-    'jquery.transit/jquery.transit.js',
-    'alias.js'
-      ], 'client');
+  api.add_files('jquery.transit/jquery.transit.js', 'client')
+  api.add_files('alias.js', 'client);
 });
