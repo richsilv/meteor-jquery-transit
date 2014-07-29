997c807e78ef4f4b191646982bdc5316626b23a6
Richard Silverton <richsilv@yahoo.co.uk>
2014-07-29 16:08:03 +0100

    Added alias reference

--- package.js
+++ package.js
@@ -3,5 +3,8 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.add_files(['jquery.transit/jquery.transit.js'], 'client');
+  api.add_files([
+    'jquery.transit/jquery.transit.js',
+    'alias.js'
+      ], 'client');
 });
